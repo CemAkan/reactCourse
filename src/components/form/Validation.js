fcbe1b92cb16f8eab6ccab46deb699e6d82f895a
CemAkan <cemakan5014@gmail.com>
2023-05-10 19:18:38 +0300

    passwordConfirm error message edited

--- src/components/form/Validation.js
+++ src/components/form/Validation.js
@@ -3,9 +3,9 @@ import * as yup from "yup";
 const validation = yup.object().shape({
   email: yup.string().email().required(),
   password: yup.string().min(6).required(),
-  confirmPassword: yup
+  passwordConfirm: yup
     .string()
-    .oneOf([yup.ref("password")])
+    .oneOf([yup.ref("password")], "Passwords must match")
     .required(),
 });
 
